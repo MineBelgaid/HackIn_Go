9c2b5e2be8f000445413c2f6d4ce7fda48ae067c
Mohammed Tirichine <mohtiri718@gmail.com>
2022-03-04 11:06:43 +0100

    Setting the env

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+const Home = () => {
+	return (
+        <section className="flex flex-col justify-around">
+            <img src="logo.png" alt="logo" />
+            <button>GET STARTED</button>
+        </section>
+    );
+};
+
+export default Home;
